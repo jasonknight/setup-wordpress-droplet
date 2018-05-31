6dcdf614374bbcf3ec5174a1f00b1573b3269784
Jason Martin <jason.martin83@protonmail.com>
2018-05-31 11:33:28 -0400

    Adding full php install
    Signed-off-by: Jason Martin <jason.martin83@protonmail.com>

--- util.php
+++ util.php
@@ -5,6 +5,6 @@ preg_match("/^\s+inet\s+(\d+\.\d+\.\d+\.\d+)/",$ifconfig[1],$matches);
 $private_ip = $matches[1];
 $free = explode("\n",shell_exec("free -m | grep 'Mem:'"));
 print_r($free);
-preg_match("/^Mem.\s+(\d+)\s+(\d+)\s+(\d+)\s(\d+)\s+(\d+)\s+(\d+)/",$free[0],$matches);
+preg_match("/Mem.\s+(\d+)\s+(\d+)\s+(\d+)\s(\d+)\s+(\d+)\s+(\d+)/",$free[0],$matches);
 print_r($matches);
 
