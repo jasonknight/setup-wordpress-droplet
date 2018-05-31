397973b69d91e8526e3d0b6062b7d37ea5e32090
Jason Martin <jason.martin83@protonmail.com>
2018-05-31 11:46:34 -0400

    Adding full php install
    Signed-off-by: Jason Martin <jason.martin83@protonmail.com>

--- util.php
+++ util.php
@@ -1,10 +1,23 @@
 <?php
-
-$ifconfig = explode("\n",shell_exec("ifconfig | grep 'inet 10'"));
-preg_match("/^\s+inet\s+(\d+\.\d+\.\d+\.\d+)/",$ifconfig[1],$matches);
-$private_ip = $matches[1];
+$matches = array();
+$ifconfig = explode("\n",@shell_exec("ifconfig | grep 'inet 10'"));
+if ( ! empty($ifconfig) && isset($ifconfig[1]) ) {
+	preg_match("/^\s+inet\s+(\d+\.\d+\.\d+\.\d+)/",$ifconfig[1],$matches);
+}
+if ( $matches && ! empty($matches) ) {
+	$private_ip = $matches[1];
+}
 $free = explode("\n",shell_exec("free -m | grep 'Mem:'"));
-print_r($free);
 preg_match("/Mem.\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)/",$free[0],$matches);
-print_r($matches);
-
+$available_memory = $matches[6];
+$base_url = "https://raw.githubusercontent.com/jasonknight/setup-wordpress-droplet/master/templates/";
+if ( isset($_SERVER['argv']) && isset($_SERVER['argv'][1] ) ) {
+	$contents = @file_get_contents($base_url . "/" . $_SERVER['argv'][1]);
+	if ( ! empty( $contents ) ) {
+		file_put_contents("/tmp/working.php");
+		include("/tmp/working.php");
+		unlink("/tmp/working.php");
+	} else {
+		echo "#empty\n";
+	}
+}
