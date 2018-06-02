e8fe9f2103925a1b547ccf69d59e5aa4faca1d2b
Jason Martin <jason.martin83@protonmail.com>
2018-06-02 11:04:59 -0400

    fixing mysql config
    Signed-off-by: Jason Martin <jason.martin83@protonmail.com>

--- debian.sh
+++ debian.sh
@@ -163,11 +163,11 @@ if [[ $@ == *"mysql"* ]] || [[ $@ == *"wordpress"* ]]; then
 	apt install -y -qq \
 		php7.2-mysql
 	# Now we have to do the same as we did for the dev, staging, etc.
+		rm "$ROOT/mysql.sh"
 		for i in 'dev' 'staging' 'production'
 		do
 			echo "Setting up $i"
 			up=${i^^}
-			rm "$ROOT/mysql.sh"
 			echo "\"CREATE DATABASE IF NOT EXISTS \$WORDPRESS_${up}_DB_NAME; GRANT all privileges on \$WORDPRESS_${up}_DB_NAME.* to '\$WORDPRESS_${up}_DB_USER'@'localhost' identified by '\$WORDPRESS_${up}_DB_PASSWORD';FLUSH PRIVILEGES;\" | mysql -u root" >> mysql.sh
 	  done
 fi
