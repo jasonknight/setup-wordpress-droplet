1b63c7dc0ca0873356ddfc9decfb9b796bd01d73
Jason Martin <jason.martin83@protonmail.com>
2018-06-05 12:06:35 -0400

    Splitting stuff off into individual files
    Signed-off-by: Jason Martin <jason.martin83@protonmail.com>

--- debian/docker.sh
+++ debian/docker.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 echo -e "Adding Docker Repository\n";
+apt install -y -qq \
+	build-essential \
+	apt-transport-https \
+	lsb-release \
+	ca-certificates \
+	gnupg2 \
+	software-properties-common 
+
 SOURCES_LINE="deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 if grep -Fxq "$SOURCES_LINE" /etc/apt/sources.list
 then
